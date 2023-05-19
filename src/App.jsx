8833432e492feaa3c5bbe61705a48f8a3b51c33d
KINTARIK <kintarikk@yandex.ru>
2023-05-19 18:33:44 +0300

    Other styles, app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import styles from "./App.module.scss";
+import "./styles";
+import { FilterMenu, Gallery, Header } from "./components";
+
+function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <div className={styles["App"] + " " + (darkTheme && styles["App--dark"])}>
+      <Header setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+      <FilterMenu darkTheme={darkTheme} />
+      <Gallery darkTheme={darkTheme} />
+    </div>
+  );
+}
+export default App;
